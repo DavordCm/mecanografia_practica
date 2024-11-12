bff27f37a57ee7d3f7132fc5439c36ac2d8782d4
Davorcm <jhoosman.xd@gmail.com>
2024-11-11 19:39:01 -0500

    Avance con mi practica de 2 dias de mecanografia

---
+++ data.js
@@ -0,0 +1,8 @@
+export const words = [
+  "a", "de", "y", "en", "que", "la", "el", "con", "por", "para", "un", "una", 
+  "como", "cuando", "esto", "pero", "más", "tengo", "hay", "esto", "todo", "si", 
+  "bueno", "gran", "porque", "al", "este", "ser", "puede", "aquí", "ahora", "sobre", 
+  "tener", "hacer", "deber", "yo", "ella", "nosotros", "vosotros", "ellos", "tiempo", 
+  "día", "noche", "persona", "gente", "trabajo", "camino", "verdad", "alguien", 
+  "algún", "nada", "todo", "nuevo", "vida", "mundo", "bien", "mal", "hombre", "mujer"
+];
